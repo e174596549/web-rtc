4ec1fa43d038de4014473151c57f30d99f26e361
yuan.liu <yuan.liu@17zuoye.com>
2019-07-04 18:12:44 +0800

    修改 APP 配置

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-var app = require('express')();
+const express = require('express');
+var app = express();
+const path = require('path');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     console.log('a user connected');
-    socket.on('offer', function(msg){
+    socket.on('offer', function(msg) {
         console.log('offer:', msg)
         socket.broadcast.emit('offer', msg);
     });
-    socket.on('answer', function(msg){
+    socket.on('answer', function(msg) {
         console.log('answer:', msg)
         socket.broadcast.emit('answer', msg);
     });
-    socket.on('candidate', function(msg){
+    socket.on('candidate', function(msg) {
         console.log('candidate:', msg)
         socket.broadcast.emit('candidate', msg);
     });
 });
 
-http.listen(3000, function(){
+http.listen(4000, function() {
     console.log('listening on *:3000');
 });
